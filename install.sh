74a5342874d26c52bc9982653648ad1bf6bad13b
Yuxi Long <longyuxi@live.com>
2022-01-07 18:35:02 -0500

    added instructions for installing

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 # clone repository
 
-git clone https://github.com/longyuxi/nvim-config
+# git clone https://github.com/longyuxi/nvim-config
+
+# patch fonts
+
+# git clone https://github.com/ryanoasis/nerd-fonts
+# ./install.sh
+
+# make neovide
+# git clone https://github.com/neovide/neovide
+# compile differently depending on the operating system
